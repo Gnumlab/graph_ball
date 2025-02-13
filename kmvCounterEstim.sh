1e14f8a818244b8c141ac45af3859045365be9db
Luca Pepè Sciarria <luca.pepesciarria@gmail.com>
2025-02-13 14:45:29 +0100

    hot fix

--- kmvCounterEstim.sh
+++ kmvCounterEstim.sh
@@ -1,12 +1,12 @@
-
+k=32
 DATASET_FOLDER=dataset/data
 EXPERIMENT_TYPE=size-estim
 RESULT_FOLDER=$DATA_FOLDER/balls
 mkdir $RESULT_FOLDER
 
-echo ./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 
-./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 
-./build/apps/run $EXPERIMENT_TYPE fb-wosn-friends 0 
-./build/apps/run $EXPERIMENT_TYPE ia-enron-email-all 1 
-./build/apps/run $EXPERIMENT_TYPE soc-youtube-growth 1
-./build/apps/run $EXPERIMENT_TYPE soc-flickr-growth 1 
+echo ./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k
+./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k
+./build/apps/run $EXPERIMENT_TYPE fb-wosn-friends 0 $k
+./build/apps/run $EXPERIMENT_TYPE ia-enron-email-all 1 $k 
+./build/apps/run $EXPERIMENT_TYPE soc-youtube-growth 1 $k
+./build/apps/run $EXPERIMENT_TYPE soc-flickr-growth 1 $k
