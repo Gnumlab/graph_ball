8cb86ed5b291892263b0346ebaf28baeb4264c10
Luca Pepè Sciarria <luca.pepesciarria@gmail.com>
2025-02-13 14:51:30 +0100

    hot fix

--- kmvCounterEstim.sh
+++ kmvCounterEstim.sh
@@ -1,12 +1,14 @@
 k=32
 DATASET_FOLDER=dataset/data
 EXPERIMENT_TYPE=size-estim
-RESULT_FOLDER=$DATA_FOLDER/balls
+RESULT_FOLDER=results/size-estim
 mkdir $RESULT_FOLDER
 
-echo ./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k
-./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k
-./build/apps/run $EXPERIMENT_TYPE fb-wosn-friends 0 $k
-./build/apps/run $EXPERIMENT_TYPE ia-enron-email-all 1 $k 
-./build/apps/run $EXPERIMENT_TYPE soc-youtube-growth 1 $k
-./build/apps/run $EXPERIMENT_TYPE soc-flickr-growth 1 $k
+echo ./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k > $RESULT_FOLDER/comm-linux-kernel-reply.csv
+
+
+./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k > $RESULT_FOLDER/comm-linux-kernel-reply.csv
+./build/apps/run $EXPERIMENT_TYPE fb-wosn-friends 0 $k > $RESULT_FOLDER/fb-wosn-friends.csv
+./build/apps/run $EXPERIMENT_TYPE ia-enron-email-all 1 $k > $RESULT_FOLDER/ia-enron-email-all.csv
+./build/apps/run $EXPERIMENT_TYPE soc-youtube-growth 1 $k > $RESULT_FOLDER/soc-youtube-growth.csv
+./build/apps/run $EXPERIMENT_TYPE soc-flickr-growth 1 $k > $RESULT_FOLDER/soc-flickr-growth.csv
