b1e2a05fa6ef753e15d682325bef67eb4c1e79b2
Alessandrostr95 <alessandrostr95@gmail.com>
2025-02-18 12:04:41 +0100

    removed kmvCounterSize.sh, replaced by kmvCounterEstim.sh

--- kmvCounterSize.sh
+++
@@ -1,9 +0,0 @@
-k=32
-DATASET_FOLDER=dataset/data
-RESULT_FOLDER=results/counter-size
-EXPERIMENT_TYPE=counter-quality
-
-mkdir $RESULT_FOLDER
-
-# gdb --args ./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k
-./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k > $RESULT_FOLDER/comm-linux-kernel-reply.csv
