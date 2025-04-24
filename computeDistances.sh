6e33bd8d41e05834842133ac1f860b2d3d450312
Alessandrostr95 <alessandrostr95@gmail.com>
2025-04-24 11:23:02 +0200

    script for running code

--- computeDistances.sh
+++ computeDistances.sh
@@ -1,9 +1,9 @@
 k=32
-DATASET_NAME=comm-linux-kernel-reply
 RESULT_FOLDER=results/distances
 EXPERIMENT_TYPE=distances
 
 mkdir $RESULT_FOLDER
 
-./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k
-
+# ./build/apps/run $EXPERIMENT_TYPE comm-linux-kernel-reply 1 $k
+./build/apps/run $EXPERIMENT_TYPE fb-wosn-friends 0 $k
+# ./build/apps/run $EXPERIMENT_TYPE ia-enron-email-all 1 $k
